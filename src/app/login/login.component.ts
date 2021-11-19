6fa427e4bc7d6031b01691f8a33192a8335f9f49
Fabrice Coulon <fabrice.coulon@consid.se>
2021-11-19 15:02:45 +0100

    change route after login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoginService } from '../login.service';
 
 @Component({
@@ -8,17 +9,22 @@ import { LoginService } from '../login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public loginService: LoginService) { }
+  constructor(public loginService: LoginService,
+    private router: Router) { 
+    
+  }
 
   ngOnInit(): void {
   }
 
   login() {
     this.loginService.loggedIn = true;
+    this.router.navigate(['/home']);
   }
 
   logout() {
     this.loginService.loggedIn = false;
+    this.router.navigate(['/login']);
   }
 
 }
