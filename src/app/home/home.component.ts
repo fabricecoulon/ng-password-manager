ed56c18dd890cad45a202a3c71b8e5e0f877efc5
Fabrice Coulon <fabrice.coulon@consid.se>
2021-11-19 14:03:08 +0100

    Working show/hide function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,10 +8,12 @@ import { PasswordEntriesService } from '../password-entries.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  entries: Entry[]; 
+  entries: Entry[];
+  showPassword: boolean[]; 
 
   constructor(public entriesSvc: PasswordEntriesService) { 
     this.entries = [];
+    this.showPassword = [];
   }
 
   ngOnInit(): void {
@@ -22,7 +24,29 @@ export class HomeComponent implements OnInit {
     // This is equivalent to promise.then(), then do a GET
     this.entriesSvc.getEntries().subscribe( entries => {
       this.entries = entries
+      this.showPassword = [];
+      this.entries.forEach( e => {
+        this.showPassword[e.id] = false;
+      })
     })
+  }
+
+  showEntryPassword(id: number): boolean {
+    if (this.showPassword[id] == null) {
+      return false;
+    }
+    let val = this.showPassword[id];
+    console.log('showEntryPassword '+id+' '+val);
+    return val;
+  }
+
+  toggleShowEntryPassword(id: number): void {
+    if (this.showPassword[id] == null) {
+      return;
+    }
+    this.showPassword[id] = !this.showPassword[id];
+    let val = this.showPassword[id];
+    console.log('toggleShowEntryPassword '+id+' '+val);
   }
 
 }
--- src/app/model/entry.ts
+++ src/app/model/entry.ts
@@ -2,12 +2,14 @@ export class Entry {
     id: number;
     date: Date;
     url: string;
+    username: string;
     password: string;
 
     constructor() {
         this.id = -1;
         this.date = new Date();
         this.url = '';
+        this.username = '';
         this.password = '';
     }
 }
