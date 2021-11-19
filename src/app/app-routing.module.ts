e303aabbd3769628ada89af180b61992850de802
Fabrice Coulon <fabrice.coulon@consid.se>
2021-11-19 11:13:29 +0100

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // !!!patchMatch full means that the entire path must be an empty string to match otherwise it matches all paths
+  { path: 'home', component: HomeComponent } // path cannot start with a slash!!!
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
