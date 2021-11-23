dd5931d58055a99eaf6ffc480bc9d79ebe968a0b
Fabrice Coulon <fabrice.coulon@consid.se>
2021-11-23 14:39:52 +0100

    Adjust the cipher test

--- crypto-js-ex01.js
+++ crypto-js-ex01.js
@@ -1,7 +1,8 @@
 const { assert } = require('console');
 const cryptojs = require('crypto-js');
 
-let key = 'secret!'; // master password as secret key
+let username = "fabrice";
+let password = "1234";
 
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
 // To be more stringent in adhering to RFC 3986 (which reserves !, ', (, ), and *), even though these 
@@ -12,9 +13,18 @@ function fixedEncodeURIComponent(str) {
     });
 }
 
-let txtMsg = "äö'ö'ä@£$€$1234"; // "12345678901234567890123456789012"; //32-bytes long
 
-let encodedTxtMsg = fixedEncodeURIComponent(txtMsg);
+// if you pass a string into btoa() containing characters that occupy more than one byte, you will get an error
+// that's why we use percent/URI encoding before, to make sure that every character uses only one byte:
+let secretPassPhrase = fixedEncodeURIComponent(password) + fixedEncodeURIComponent(username);
+secretPassPhrase = btoa(secretPassPhrase);  // MTIzNGZhYnJpY2U=
+console.log('secretPassPhrase', secretPassPhrase);
+
+let key = secretPassPhrase; // master password as secret key
+
+let txtMsg = "testurl1";
+
+let encodedTxtMsg = btoa(fixedEncodeURIComponent(txtMsg));
 console.log('encodedTxtMsg', encodedTxtMsg);
 
 let ciphertext = cryptojs.AES.encrypt(encodedTxtMsg, key).toString();
@@ -24,7 +34,7 @@ let bytes = cryptojs.AES.decrypt(ciphertext, key);
 let originaltext = bytes.toString(cryptojs.enc.Utf8);
 console.log('originaltext', originaltext);
 
-let decodedTxtMsg = decodeURIComponent(originaltext);
+let decodedTxtMsg = decodeURIComponent(atob(originaltext));
 console.log('decodedTxtMsg', decodedTxtMsg);
 
 console.log(txtMsg === decodedTxtMsg); 
